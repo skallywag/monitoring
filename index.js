afa03ceb371b0915b68091ff149e084a14527350
Samuel Lee <lee.sam1492@gmail.com>
2021-12-16 11:32:16 -0700

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 const port = process.env.PORT || 4545
